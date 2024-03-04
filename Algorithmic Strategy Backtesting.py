a22386fbbda45c352b5742d142c1069be975d051
Kesav_P <161378031+KesavP-01@users.noreply.github.com>
2024-03-04 17:01:18 -0500

    Add files via upload

---
+++ Algorithmic Strategy Backtesting.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep  3 11:26:14 2023
+
+@author: KESAV
+"""
+import numpy as np
+import pandas as pd
+import yfinance as yf
+import datetime as dt
+import copy
+import matplotlib.pyplot as plt
+
+
+def CAGR(DF):
+    "function to calculate the Cumulative Annual Growth Rate of a trading strategy"
+    df = DF.copy()
+    df["cum_return"] = (1 + df["mon_ret"]).cumprod()
+    n = len(df)/12
+    CAGR = (df["cum_return"].tolist()[-1])**(1/n) - 1
+    return CAGR
+
+def volatility(DF):
+    "function to calculate annualized volatility of a trading strategy"
+    df = DF.copy()
+    vol = df["mon_ret"].std() * np.sqrt(12)
+    return vol
+
+def sharpe(DF,rf):
+    "function to calculate sharpe ratio ; rf is the risk free rate"
+    df = DF.copy()
+    sr = (CAGR(df) - rf)/volatility(df)
+    return sr
+    
+
+def max_dd(DF):
+    "function to calculate max drawdown"
+    df = DF.copy()
+    df["cum_return"] = (1 + df["mon_ret"]).cumprod()
+    df["cum_roll_max"] = df["cum_return"].cummax()
+    df["drawdown"] = df["cum_roll_max"] - df["cum_return"]
+    df["drawdown_pct"] = df["drawdown"]/df["cum_roll_max"]
+    max_dd = df["drawdown_pct"].max()
+    return max_dd
+
+# Download historical data (monthly) for DJI constituent stocks
+
+tickers = ["AXP","AMGN","AAPL","BA","CAT","CSCO","CVX","GS","HD","HON","IBM","INTC","JNJ","KO","JPM","MCD","MMM","MRK","MSFT","NKE","PG","TRV","UNH","CRM","VZ","V","WBA","WMT","DIS","DOW"]
+
+ohlc_mon = {} # directory with ohlc value for each stock            
+start = dt.datetime.today()-dt.timedelta(1460)
+end = dt.datetime.today()
+
+for ticker in tickers:
+    ohlc_mon[ticker] = yf.download(ticker,start,end,interval='1mo')
+    ohlc_mon[ticker].dropna(inplace=True,how="all")
+    
+tickers = ohlc_mon.keys()
+
+ohlc_dict = copy.deepcopy(ohlc_mon)
+return_df = pd.DataFrame()
+for ticker in tickers:
+    print("calculating monthly return for ",ticker)
+    ohlc_dict[ticker]["mon_ret"] = ohlc_dict[ticker]["Adj Close"].pct_change()
+    return_df[ticker] = ohlc_dict[ticker]["mon_ret"]
+return_df.dropna(inplace=True)
+
+
+def pflio(DF, n, x):
+    df = DF.copy()
+    portfolio = []
+    monthly_ret = [0]
+    for i in range(1, len(df)):
+        if len(portfolio) > 0:
+            monthly_ret.append(df[portfolio].iloc[i, :].mean())
+            bad_stocks = df[portfolio].iloc[i, :].sort_values(ascending = True)[:x].index.values.tolist()
+            portfolio = [t for t in portfolio if t not in bad_stocks]
+        fill = n - len(portfolio)
+        new_picks = df[[t for t in tickers if t not in portfolio]].iloc[i, :].sort_values(ascending = False)[:fill].index.values.tolist()
+        portfolio = portfolio + new_picks
+        print(portfolio)
+    monthly_ret_df = pd.DataFrame(np.array(monthly_ret), columns = ["mon_ret"])
+    return monthly_ret_df
+
+
+CAGR(pflio(return_df, 4,2))
+volatility(pflio(return_df, 6 , 3))
+sharpe(pflio(return_df, 6, 3), 0.025)
+max_dd(pflio(return_df, 6 , 3))
+
+DJI = yf.download("^DJI",dt.date.today()-dt.timedelta(1460),dt.date.today(),interval='1mo')
+DJI["mon_ret"] = DJI["Adj Close"].pct_change().fillna(0)
+CAGR(DJI)
+sharpe(DJI,0.025)
+max_dd(DJI)
+
+plt.figure(dpi=600)
+fig, ax = plt.subplots()
+plt.plot((1 + pflio(return_df,7,5)).cumprod())
+plt.plot((1+DJI["mon_ret"].reset_index(drop=True)).cumprod())
+plt.title("Index Return vs Strategy Return")
+plt.ylabel("cumulative return")
+plt.xlabel("months")
+ax.legend(["Strategy Return","Index Return"])
+
+
+        
+        
+        
+    
+
+
